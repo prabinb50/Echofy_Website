312ef083233ab88ae41a05fb8c2384cc670625fe
Prabin Joshi <joshiprabin17@gmail.com>
2025-07-24 20:55:53 +0545

    updated code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='bg-green-400 text-center text-2xl'>Let's Start</h1>
+    </div>
+  )
+}
