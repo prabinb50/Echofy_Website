b350c64de40979b8ce1883b1dc50448f4d2acd99
Prabin Joshi <joshiprabin17@gmail.com>
2025-07-28 13:08:51 +0545

    Refactor App component structure and update AboutImage styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import React from 'react'
 import BannerImage from './components/BannerImage'
 import AboutImage from './components/AboutImage'
+import Scroll from './components/Scroll'
 
 export default function App() {
   return (
     <div>
       <BannerImage />
-      <AboutImage/>
+      <Scroll />
+      <AboutImage />
     </div>
   )
 }
